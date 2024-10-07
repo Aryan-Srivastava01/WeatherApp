cc94e816fb508dcd3c94bca10d8dbd7c57491e61
Aryan-srivastava <104515417+Aryan-Srivastava01@users.noreply.github.com>
2024-10-07 18:00:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Clone the project
+
+type npm i in terminal
+
+Go to OpenWeather and get your api key 
+Type your API key in apikey.js file
+
+now run the project in terminal - npm start
+
+
+
---
+++ src/apikeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "63c07159f2f9627d27951e66b18edd6c",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
